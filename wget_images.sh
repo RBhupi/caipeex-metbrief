5796d008f2578f12f33c91695607e59f0485b488
Bhupendra Raut <bhupendra.raut@gmail.com>
2018-07-03 17:42:09 +0530

    First test run; many images added, changed;draft

--- wget_images.sh
+++ wget_images.sh
@@ -5,45 +5,49 @@
 mkdir fig
 cd fig
 
-wget -nc http://imdpune.gov.in/Seasons/Pre_Monsoon/onset_SW.gif
-convert onset_SW.gif onset_SW.png
+#Get all the Figures in ./fig Directory
+#wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/onset_SW.gif
+#convert onset_SW.gif onset_SW.png
 
-wget -nc http://imdpune.gov.in/Seasons/Pre_Monsoon/seasonal-rain.jpg
+wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/seasonal-rain.jpg
 
-wget -nc http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/concwinds925.gif
+wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/concwinds925.gif
 convert concwinds925.gif concwinds925.png
 
-wget -nc http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/aod10days.gif
+wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/aod10days.gif
 convert aod10days.gif aod10days.png 
 
-wget -nc http://www.ncmrwf.gov.in/Data/mihir/Soil-Moisture/soilm_7days.gif
+wget -N http://www.ncmrwf.gov.in/Data/mihir/Soil-Moisture/soilm_7days.gif
 convert soilm_7days.gif soilm_7days.png 
 
-wget -nc http://imdpune.gov.in/Seasons/Pre_Monsoon/all-india.gif
+wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/all-india.gif
 convert all-india.gif all-india.png
-wget -nc http://imdpune.gov.in/Seasons/Pre_Monsoon/homo.gif 
+wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/homo.gif 
 convert homo.gif homo.png 
+
+wget -N http://nwp.imd.gov.in/gefs/mslp_avg_spr-24.png
 
 #Download Yesterday's data for todays forecast fields from NCMRWF
 DATE=`date -d "yesterday 13:00" '+%Y-%m-%d'`
 wget -nc http://www.ncmrwf.gov.in/Data/mihir/$DATE/NCUM-Outputs/Temperature/ncum_fcst_Tmax_day-1.png
 wget -nc http://www.ncmrwf.gov.in/Data/mihir/$DATE/NCUM-Outputs/Temperature/ncum_fcst_Tmin_day-1.png
-wget -nc http://www.ncmrwf.gov.in/Data/mihir/Temp-Tendency/ncum_fcst_Tmax_tend.png
-wget -nc http://www.ncmrwf.gov.in/Data/mihir/Temp-Tendency/ncum_fcst_Tmin_tend.png
+#wget -nc http://www.ncmrwf.gov.in/Data/mihir/Temp-Tendency/ncum_fcst_Tmax_tend.png
+#wget -nc http://www.ncmrwf.gov.in/Data/mihir/Temp-Tendency/ncum_fcst_Tmin_tend.png
 
 
 #download satellite products from IMD 
-wget -nc satellite.imd.gov.in/img/3Dasiasec_vis.jpg
-wget -nc satellite.imd.gov.in/img/3Dasiasec_ctbt.jpg
-wget -nc http://satellite.imd.gov.in/img/3Dtpw_a1.jpg 
-wget -nc http://satellite.imd.gov.in/img/3D_shear.jpg 
-wget -nc http://satellite.imd.gov.in/img/3D_shten.jpg 
+wget -nc http://satellite.imd.gov.in/img/3DRasiasec_vis.jpg
+#wget -nc satellite.imd.gov.in/img/3Dasiasec_ctbt.jpg
+wget -nc http://satellite.imd.gov.in/img/3DRasiasec_bt1.jpg
+#wget -nc http://satellite.imd.gov.in/img/3Dtpw_a1.jpg 
+#wget -nc http://satellite.imd.gov.in/img/3D_shear.jpg 
+#wget -nc http://satellite.imd.gov.in/img/3D_shten.jpg 
 wget -nc http://satellite.imd.gov.in/img/3D_midsh.jpg 
 wget -nc http://satellite.imd.gov.in/img/3D_lowcon.jpg 
 wget -nc http://satellite.imd.gov.in/img/3D_updiv.jpg 
 #wget -nc http://satellite.imd.gov.in/img/3D_updiv.jpg 
 #wget -nc http://satellite.imd.gov.in/img/3Dasianew.jpg 
-wget -nc http://satellite.imd.gov.in/dynamic/satbltn.pdf 
+
 
 wget -nc http://satellite.imd.gov.in/img/SOLAPUR.gif
 convert SOLAPUR.gif SOLAPUR.png
@@ -64,19 +68,41 @@ wget -nc http://nwp.imd.gov.in/wrf/WRFd02-ZWd850_06.png
 wget -nc http://nwp.imd.gov.in/wrf/WRFd02-ZWd200_06.png
 wget -nc http://nwp.imd.gov.in/wrf/WRFd02-ZWd500_06.png
 
+#IMD Meteogram
+wget -N http://nwp.imd.gov.in/gfs/gfs_meteograms/SLP-meteogram.gif
+convert SLP-meteogram.gif SLP-meteogram.png
+wget -N http://nwp.imd.gov.in/gfs/gfs_meteograms/AGD-meteogram.gif
+convert AGD-meteogram.gif AGD-meteogram.png
+
+
 #Solapur WRF forecast from Gayatri
 DATE=`date -d "today 13:00" '+%Y%m%d'`
+
 wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/skew/Solapur.000010.png
-wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/skew/Tuljapur.000010.png
-wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/cape/cape.000010.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/skew/Aurangabad.000010.png
+
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/skew/Solapur.000012.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/skew/Aurangabad.000012.png
+
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/cape/cape.000013.png
 wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/cine/cine.000013.png
 wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/mdbz/mdbz.000013.png
 wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/WLD/wld.000013.png
 wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/precit/precipt.000012.png
-wget http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/lclh/lcl.000013.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/lclh/lcl.000013.png
+
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/cape/cape.000015.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/cine/cine.000015.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/mdbz/mdbz.000015.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/WLD/wld.000015.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/precit/precipt.000014.png
+wget -nc http://103.251.186.33/~caipeex/wrf/$DATE-18/d3/lclh/lcl.000015.png
+
+#Get Solapur data figures 
 
 
 
 cd ..
-
+wget -N http://www.imd.gov.in/section/nhac/dynamic/allindianew.pdf
+wget -N http://satellite.imd.gov.in/dynamic/satbltn.pdf 
 pdflatex metbrief.tex
