d60070fa324edf9ff36eeaab70627ca09f58bdc8
Bhupendra <bhupendraraut@gmail.com>
2019-06-19 12:12:49 +0530

    my Second metbrief modified

--- wget_images.sh
+++ wget_images.sh
@@ -13,21 +13,25 @@ convert onset_SW.gif onset_SW.png
 
 wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/seasonal-rain.jpg
 
-#wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/concwinds925.gif
+#wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/concwinds925.gif #fsk467
 #convert concwinds925.gif concwinds925.png
 
-#wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/aod10days.gif
+#wget -N http://www.ncmrwf.gov.in/Data/mihir/Dust-Forecast/aod10days.gif #<BS>
 #convert aod10days.gif aod10days.png 
 
-#wget -N http://www.ncmrwf.gov.in/Data/mihir/Soil-Moisture/soilm_7days.gif
+#wget -N http://www.ncmrwf.gov.in/Data/mihir/Soil-Moisture/soilm_7days.gif #<Left>
 #convert soilm_7days.gif soilm_7days.png 
 
-wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/all-india.gif
-convert all-india.gif all-india.png
-wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/homo.gif 
-convert homo.gif homo.png 
+DATE=`date -d "yesterday 13:00" '+%Y%m%d000000000'`
+wget -N wget http://image.nmc.cn/product/2019/06/18/NWPR/SEVP_NCEP_NWPR_SNCEP_ECONC_ASIA_L88_PB_$DATE.gif   
+convert SEVP_NCEP_NWPR_SNCEP_ECONC_ASIA_L88_PB_$DATE.gif dust.jpg
 
-wget -N http://nwp.imd.gov.in/gefs/mslp_avg_spr-24.png
+#wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/all-india.gif
+#convert all-india.gif all-india.png
+#wget -N http://imdpune.gov.in/Seasons/Pre_Monsoon/homo.gif 
+#convert homo.gif homo.png 
+
+#wget -N http://nwp.imd.gov.in/gefs/mslp_avg_spr-24.png
 
 #Download Yesterday's data for todays forecast fields from NCMRWF
 DATE=`date -d "yesterday 13:00" '+%Y-%m-%d'`
@@ -38,9 +42,12 @@ DATE=`date -d "yesterday 13:00" '+%Y-%m-%d'`
 
 
 #download satellite products from IMD 
-wget -nc http://satellite.imd.gov.in/img/3DRasiasec_vis.jpg
+#wget -nc http://satellite.imd.gov.in/img/3DRasiasec_vis.jpg
 wget -nc satellite.imd.gov.in/img/3Dasiasec_ctbt.jpg
 wget -nc http://satellite.imd.gov.in/img/3Dhum_prof620_a1.jpg
+wget -nc http://satellite.imd.gov.in/img/3Dswsec_swir.jpg
+
+
 #wget -nc http://satellite.imd.gov.in/img/3DRasiasec_bt1.jpg
 #wget -nc http://satellite.imd.gov.in/img/3Dtpw_a1.jpg 
 #wget -nc http://satellite.imd.gov.in/img/3D_shear.jpg 
@@ -56,14 +63,29 @@ wget -nc http://satellite.imd.gov.in/img/3Dhum_prof620_a1.jpg
 #convert SOLAPUR.gif SOLAPUR.png
 
 #IMD forecast Product
-wget -nc http://nwp.imd.gov.in/wrf/WRFd01-CAPE_06.png 
-wget -nc http://nwp.imd.gov.in/wrf/WRFd01-CIN_06.png
-wget -nc http://nwp.imd.gov.in/wrf/WRFd01-LCL_06.png 
+#wget -nc http://nwp.imd.gov.in/wrf/WRFd01-CAPE_06.png 
+#wget -nc http://nwp.imd.gov.in/wrf/WRFd01-CIN_06.png
+#wget -nc http://nwp.imd.gov.in/wrf/WRFd01-LCL_06.png 
 
 
 #GFS Forecast
-wget -N http://nwp.imd.gov.in/gefs/con-spr-wind_zonal850-0.png
-wget -N http://nwp.imd.gov.in/gefs/con-spr-wind_zonal200-0.png
+wget -N http://nwp.imd.gov.in/gfs/06hgfs_mslp.gif
+convert 06hgfs_mslp.gif 06hgfs_mslp.jpg
+
+wget -N http://nwp.imd.gov.in/gfs/06hgfs_850wind.gif
+convert 06hgfs_850wind.gif 06hgfs_850wind.jpg
+
+wget -N http://nwp.imd.gov.in/gfs/06hgfs_500wind.gif
+convert 06hgfs_500wind.gif 06hgfs_500wind.jpg
+
+wget -N http://nwp.imd.gov.in/gfs/06hgfs_200wind.gif
+convert 06hgfs_200wind.gif 06hgfs_200wind.jpg
+
+#wget -N http://nwp.imd.gov.in/gfs/00hgfs_mslp.gif
+#convert 00hgfs_mslp.gif 00hgfs_mslp.jpg
+#wget -N http://nwp.imd.gov.in/gefs/con-spr-wind_zonal850-0.png
+#wget -N http://nwp.imd.gov.in/gefs/con-spr-wind_zonal500-0.png
+#wget -N http://nwp.imd.gov.in/gefs/con-spr-wind_zonal200-0.png
 
 #IMD WRF forecast
 wget -nc http://nwp.imd.gov.in/wrf/WRFd02-03-RAIN_06.png
@@ -85,13 +107,13 @@ wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/skew/Solapur.000013.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/skew/Solapur.000015.png
 
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cape/cape.000015.png
-wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cine/cine.000015.png
+#wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cine/cine.000015.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/mdbz/mdbz.000015.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/WLD/wld.000015.png
 #wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/precit/precipt.000018.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/lclh/lcl.000015.png
 
-#wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cape/cape.000021.png
+wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cape/cape.000017.png
 #wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/cine/cine.000021.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/mdbz/mdbz.000017.png
 wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/WLD/wld.000017.png
@@ -106,7 +128,11 @@ wget -N http://103.251.186.33/~caipeex/wrf/$DATE-18/lwp/lwp_iwp.000014.png
 #Get Solapur data figures
 cd ../fig_obs
 DATE=`date -d "today 13:00" '+%Y%m%d'`
-#wget -N http://103.251.186.33/\~caipeex/solapur/rsonde/skewT_$DATE.png
+wget -N http://www.imd.gov.in/section/dwr/img/caz_slp.gif
+convert caz_slp.gif caz_slp.jpg
+
+wget -N http://www.imd.gov.in/section/dwr/img/vp2_slp.gif
+convert vp2_slp.gif vp2_slp.jpg
 
 wget http://103.251.186.33/\~caipeex/radarimages/radar_images2/$DATE/2019060703095301dBZ.ppi.png -O radarppi.png
 
